21cc607c5a503b0c9fd3d3cc81ff0f2b035e21ea
Stanley Fils <stanleyfils@gmail.com>
2020-06-26 23:15:48 -0400

    Initial commit

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -24,7 +24,7 @@ const LandingPage = () => {
       <HeaderImage src={BannerImage} alt="animated chefs cooking" />
     </Container>
     // <section>
-    //     <SearchBar />
+    //     <SearchBar />git rem
     // </section>
   );
 };
